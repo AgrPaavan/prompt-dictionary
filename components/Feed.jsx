a314ee780df4e97345c2c6b7209071df6ae30b73
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2023-09-18 00:10:36 +0530

    Remove error

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,13 +7,14 @@ import PromptCard from "@components/PromptCard";
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
-      {data.map((post) => (
-        <PromptCard
-          key={post._id}
-          post={post}
-          handleTagClick={handleTagClick}
-        />
-      ))}
+      {data &&
+        data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        ))}
     </div>
   );
 };
